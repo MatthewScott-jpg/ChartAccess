1d96ddb62bcc8d04be9b6490d0adcf4db21814b2
MatthewScott-jpg <scottydotty333@gmail.com>
2021-04-14 21:52:33 -0400

    basic line chart and scatter plot handler, can now manage data as csv or json

--- chart-to-text/src/App.js
+++ chart-to-text/src/App.js
@@ -2,26 +2,42 @@ import {Vega} from 'react-vega';
 import { compile } from "vega-lite/build/vega-lite";
 import React, {useState} from "react";
 import css from './App.module.css';
+import FileLoader from './FileLoader.js';
 
 function App() {
   const [inputSpec, setInputSpec] = useState({});
   const [orgSpec, setOrgSpec] = useState({});
   const [showValues, setShowValues] = useState(false);
+  const [dragging, setDragging] = useState(false); // to show a dragging effect
 
   //Getter function for obtaining aspects of the final Vega spec
   function getInfo(){
-    const mark = inputSpec.marks[0].style[0];
-    var xTitle = inputSpec.axes[1].title;
-    var yTitle = inputSpec.axes[2].title;
+    const mark = inputSpec.marks[0].style[0]; //currently this works, have yet to find multiple mark case
+    var xTitle = "";
+    var yTitle = "";
     var style = "";
 
+    for (var i in inputSpec.axes){
+      var axe = inputSpec.axes[i];
+      if (axe.scale === 'x' & 'title' in axe) {
+        xTitle = axe.title;
+        xTitle = xTitle.replace(/_/g, ' ');
+      }
+      else if (axe.scale === 'y' & 'title' in axe) {
+        yTitle = axe.title;
+        yTitle = yTitle.replace(/_/g, ' ');
+      }
+    }
+
+    //temporary fix for vega spec modifications to variables
     if (xTitle.includes("binned") && orgSpec.encoding.x.hasOwnProperty("field")){
       xTitle = orgSpec.encoding.x.field;
     }
 
-    if (mark === "bar"){
-      style = getBarType()
-    }
+    if (mark === "bar") style = getBarType();
+    else if (mark === "point" | mark === "circle") style = getScatterType();
+    else if (mark === "line") style = getLineType();
+
     const sent1 = "This is a ".concat(style);
     const sent2 = ". Its x axis is ".concat(xTitle);
     const sent3 = ". Its y axis is ".concat(yTitle);
@@ -42,53 +58,90 @@ function App() {
   function getBarType(){
     if (orgSpec.encoding.x.hasOwnProperty("bin")){
       if (orgSpec.encoding.x.bin === true){
-        return "Histogram";
+        return "histogram";
       }
     }
     else{
-      return "Bar Chart";
+      return "bar chart";
     }
   }
 
-  //Reveals spec values on file submission
-  function handleSubmitJSON(e){
-    setShowValues(true);
-    e.preventDefault();
+  function getScatterType(){
+    return "scatter plot";
+  }
 
+  function getLineType(){
+    return "line chart";
+  }
+
+  //handlers for file dragging
+  function handleFileDragEnter(e){
+    setDragging(true);
+  }
+  function handleFileDragLeave(e){
+    setDragging(false);
+  }
+  //parses json file on drop, returns vega and vega-lite specs
+  async function handleFileDrop(e){
+    if (e.dataTransfer.types.includes('Files')===false){
+			return;
+    }
+    if (e.dataTransfer.files.length>=1){
+      let file = e.dataTransfer.files[0];
+      if (file.size>1000000){// larger than 1 MB
+        return;
+      }
+      setShowValues(false);
+      if (file.type.match(/json.*/)){
+				let reader = new FileReader();			
+				reader.onload = async (e) => {
+          const text = (e.target.result);
+          const liteSpec = JSON.parse(text);
+          if (liteSpec.data.hasOwnProperty("url")){
+            liteSpec.data.values = await getURLJSON(liteSpec.data.url);
+          }
+          const vegaspec = compile(liteSpec).spec;
+          setInputSpec(vegaspec);
+          setOrgSpec(liteSpec);
+          setShowValues(true);
+				};
+				reader.readAsText(file);
+			}
+    }
+    setDragging(false);
+    e.preventDefault();
+    
   }
 
   //obtains data from input url
   async function getURLJSON(url){
     const response = await fetch(url);
-    const data = await response.json();
+    var data = '';
+    if (url.endsWith('.csv')){
+      data = await response.text();
+      data = transform(data);
+    }
+    else if (url.endsWith('.json')){
+      data = await response.json();
+    }
+    console.log(data)
     return data;      
   }
 
-  //reads vega file, handles data in url case, sets vega-lite and vega state objects
-  async function showFile(e){
-    setShowValues(false);
-    const reader = new FileReader()
-    reader.onload = async (e) => { 
-      const text = (e.target.result)
-      const liteSpec = JSON.parse(text);
-      if (liteSpec.data.hasOwnProperty("url")){
-        liteSpec.data.values = await getURLJSON(liteSpec.data.url);
-      }
-      const vegaspec = compile(liteSpec).spec;
-      console.log(vegaspec)
-      setInputSpec(vegaspec);
-      setOrgSpec(liteSpec);
-    };
-    reader.readAsText(e.target.files[0])
+  function transform(str) {
+    let data = str.split('\n').map(i=>i.split(','));
+    let headers = data.shift();
+    let output = data.map(d=>{
+      var obj = {};
+      headers.map((h,i)=>obj[headers[i]] = d[i]);
+      return obj;
+    });
+    return output
   }
   
   return (
     <div className={css.App}>
       <main>
-        <form onSubmit={handleSubmitJSON}>
-          <input type="file" name="sourcejson" onChange={(e) => showFile(e)}/>
-          <button type="submit">Add Row</button>
-        </form>
         {
           showValues?
           (
@@ -102,6 +155,17 @@ function App() {
           </div>
           ):null
         }
+
+      <div className={css.photo}>
+          <div className={css.message}>Drop your file</div>
+            <FileLoader
+              onDragEnter={handleFileDragEnter}
+              onDragLeave={handleFileDragLeave}
+              onDrop={handleFileDrop}
+            >
+              <div className={[css.dropArea, dragging?css.dragging:null].join(' ')}></div>
+            </FileLoader>
+        </div>
       </main>
     </div>
   );
